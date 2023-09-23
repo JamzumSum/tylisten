3d5cf936bc38d1565c0baad0f720defe63a5e186
JamzumSum <zzzzss990315@gmail.com>
2023-09-23 14:53:12 +0800

    fix: all impls refers to the original list

--- tests/test_hookspec.py
+++ tests/test_hookspec.py
@@ -19,49 +19,51 @@ async def boom(a): raise RuntimeError
 # fmt: on
 
 
+@pytest.fixture
+def hook():
+    return test_message.new()
+
+
 class TestHookSpec:
-    async def test_results(self):
-        test_message.impls.clear()
-        assert not test_message.has_impl
+    async def test_results(self, hook):
+        assert not hook.has_impl
 
         async def async_add(a: int):
             return a + 2
 
-        test_message.add_impl(lambda a: a - 1)
-        test_message.add_impl(boom)
-        test_message.add_impl(lambda a: a + 1)
-        test_message.add_impl(aboom)
-        test_message.add_impl(async_add)
+        hook.add_impl(lambda a: a - 1)
+        hook.add_impl(boom)
+        hook.add_impl(lambda a: a + 1)
+        hook.add_impl(aboom)
+        hook.add_impl(async_add)
 
-        assert test_message.has_impl
-        o = await test_message.results(4)
+        assert hook.has_impl
+        o = await hook.results(4)
         assert o == [3, 5, 6]
 
-    async def test_first(self):
-        test_message.impls.clear()
-        assert not test_message.has_impl
+    async def test_first(self, hook):
+        assert not hook.has_impl
 
         async def async_add(a: int):
             return a + 2
 
-        test_message.add_impl(boom)
-        test_message.add_impl(aboom)
-        test_message.add_impl(async_add)
-        test_message.add_impl(lambda a: a + 1)
-        test_message.add_impl(lambda a: a - 1)
+        hook.add_impl(boom)
+        hook.add_impl(aboom)
+        hook.add_impl(async_add)
+        hook.add_impl(lambda a: a + 1)
+        hook.add_impl(lambda a: a - 1)
 
-        assert test_message.has_impl
-        assert 6 == await test_message.first(4)
+        assert hook.has_impl
+        assert 6 == await hook.first(4)
 
-    async def test_call(self):
-        test_message.impls.clear()
-        assert not test_message.has_impl
+    async def test_call(self, hook):
+        assert not hook.has_impl
 
-        test_message.add_impl(boom)
-        test_message.add_impl(aboom)
+        hook.add_impl(boom)
+        hook.add_impl(aboom)
 
-        assert test_message.has_impl
-        assert 0 == await test_message(4)
+        assert hook.has_impl
+        assert 0 == await hook(4)
 
-        test_message.add_impl(lambda a: a - 1)
-        assert 3 == await test_message(4)
+        hook.add_impl(lambda a: a - 1)
+        assert 3 == await hook(4)
